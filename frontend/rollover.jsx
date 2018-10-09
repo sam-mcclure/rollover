ca84358ac8b8043ecb1b644edc713ae6fc3be15a
Sam McClure <samkmcclure@gmail.com>
2018-10-09 15:17:46 -0700

    Add all reducers for user auth

--- frontend/rollover.jsx
+++ frontend/rollover.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { login, logout, signup } from './util/session_api_util';
+import configureStore from './store/store';
 
-//testing
-window.login = login;
-window.signup = signup;
-window.logout = logout;
-//testing
+//Testing
+import { login, logout, signup } from './util/session_api_util';
+//Testing
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+
+  //testing
+  window.login = login;
+  window.signup = signup;
+  window.logout = logout;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  //testing
+
   const root = document.getElementById('root');
   ReactDOM.render(<h1>rollover</h1>, root);
 });
