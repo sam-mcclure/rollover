a7ae7168bc19a7be342e2aed410f584c7c1ddb1e
Sam McClure <samkmcclure@gmail.com>
2018-10-09 13:58:50 -0700

    Create working user auth backend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?
+
+  private
+
+  def login(user)
+    @current_user = user
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def current_user
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def logout
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    @current_user = nil
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_logged_in
+    render json: ["Unauthorized"], status: 401 unless logged_in?
+  end
+
 end
