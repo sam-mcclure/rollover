deec15b19ec84b1bade83350e6fb3d0bd26a2d4c
Sam McClure <samkmcclure@gmail.com>
2018-10-10 17:11:04 -0700

    fix syntax error

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-Jbuilder.key_format camelize: :lower_left_paintbrush: 
+Jbuilder.key_format camelize: :lower
