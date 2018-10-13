71ceb86f0e971dbfa61bbd39321e96c13de7826e
Sam McClure <samkmcclure@gmail.com>
2018-10-12 17:24:54 -0700

    Start working on adding edit/delete dropdown

--- frontend/components/post/post_index_item.jsx
+++ frontend/components/post/post_index_item.jsx
@@ -1,41 +1,53 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const PostIndexItem = ({post, deletePost, currentUser}) => {
-  const postButtons = (currentUser.id === post.authorId) ?
-  <div>
-    <Link to={`posts/${post.id}/edit`}><button>Edit</button></Link>
-    <button onClick={() => deletePost(post.id)}>Delete</button>
-  </div> : '';
+class PostIndexItem extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {dropdown: 'hidden'};
+  }
 
-  const image = (post.photoUrl) ?
-    <img src={post.photoUrl} /> : '';
+  render({currentUser, post, deletePost} = this.props){
+    const postButtons = (currentUser.id === post.authorId) ?
+    <div className="dropdown-container">
+      <i className="fa fa-cog cog-icon"></i>
 
-  const video = (post.videoUrl) ?
-    <video height="240" controls>
-      <source src={post.videoUrl} />
-    </video> : '';
-
-  const audio = (post.audioUrl) ?
-    <audio controls>
-      <source src={post.audioUrl} />
-    </audio> : '';
-
-  return (
-    <div className="post-container">
-      <img className="user-img"
-        src={post.authorPhotoUrl} />
-      <div className={`post post-${post.postType}`}>
-        <strong>{post.authorUsername}</strong>
-        {post.title}
-        {image}
-        {video}
-        {audio}
-        {post.body}
-        {postButtons}
+      <div className="dropdown">
+        <Link to={`posts/${post.id}/edit`}><button>Edit</button></Link>
+        <button onClick={() => deletePost(post.id)}>Delete</button>
       </div>
-    </div>
-  );
-};
+    </div> : '';
+
+    const image = (post.photoUrl) ?
+      <img src={post.photoUrl} /> : '';
+
+    const video = (post.videoUrl) ?
+      <video height="240" controls>
+        <source src={post.videoUrl} />
+      </video> : '';
+
+    const audio = (post.audioUrl) ?
+      <audio controls>
+        <source src={post.audioUrl} />
+      </audio> : '';
+
+    return (
+      <div className="post-container">
+        <img className="user-img"
+          src={post.authorPhotoUrl} />
+        <div className={`post post-${post.postType}`}>
+          <strong className="username">{post.authorUsername}</strong>
+          {post.title}
+          {image}
+          {video}
+          {audio}
+          {post.body}
+          {postButtons}
+        </div>
+      </div>
+    );
+  }
+
+}
 
 export default PostIndexItem;
